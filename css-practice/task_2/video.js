081d8fc132e46a30614ab7942988989662dce8a9
YarynaD <ydemkiv@griddynamics.com>
2018-12-05 17:01:12 +0200

    modules

--- css-practice/task_2/video.js
+++ css-practice/task_2/video.js
@@ -1,14 +1,24 @@
-const ppbutton = document.getElementById('vidbutton');
-ppbutton.addEventListener('click', playPause);
-const myVideo = document.getElementById('myvid');
-function playPause() {
-  if (myVideo.paused) {
-    myVideo.play();
-    ppbutton.classList.add('play');
-    ppbutton.classList.remove('pause');
-  } else {
-    myVideo.pause();
-    ppbutton.classList.remove('play');
-    ppbutton.classList.add('pause');
+const videoModule = (function() {
+  const ppbutton = document.getElementById('vidbutton');
+  const myVideo = document.getElementById('myvid');
+
+  function playPause() {
+    if (myVideo.paused) {
+      myVideo.play();
+      ppbutton.classList.add('play');
+      ppbutton.classList.remove('pause');
+    } else {
+      myVideo.pause();
+      ppbutton.classList.remove('play');
+      ppbutton.classList.add('pause');
+    }
   }
-}
+
+  return {
+    init: () => {
+      ppbutton.addEventListener('click', playPause);
+    },
+  };
+})();
+
+videoModule.init();
