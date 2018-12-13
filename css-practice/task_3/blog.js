2f2b1e5ff09c2695c1a842493fd6b6e7f130b06b
YarynaD <ydemkiv@griddynamics.com>
2018-12-13 11:17:25 +0200

    fetch fix

--- css-practice/task_3/blog.js
+++ css-practice/task_3/blog.js
@@ -1,73 +1,96 @@
-const eventsSection = document.querySelector('.events-list');
-const footerSection = document.querySelector('.footer-blogs');
-
-function RenderBlogItem(model, parsedDate) {
-  const {previewImg, title, description, watched, comments} = model;
+/* global fetch, document*/
+function renderArticleItem(model) {
+  const {previewImg, title, description, watched, comments, published} = model;
   const element = document.createElement('div');
   element.classList.add('event');
+  const {month, day} = parseDate(published);
 
   const blogContent = `
-        <img class="blog-image" src=${previewImg} alt="photo">
-        <h6>${title}</h6>
-        <p>${description}</p>
-        <div class="counter">
-            <img src="img/VIEW.png" alt="icon">
-            <p>${watched}</p>
-            <img src="img/SPEECH_BUBBLE_2.png" alt="icon">
-            <p>${comments}</p>
-        </div>
-        <div class="date">
-            <p class="big-num">${parsedDate[2]}</p>
-            <p>${parsedDate[1]}</p>
-        </div>
-    `;
+         <img class="blog-image" src=${previewImg} alt="photo">
+         <h6>${title}</h6>
+         <p>${description}</p>
+         <div class="counter">
+             <img src="img/VIEW.png" alt="icon">
+             <p>${watched}</p>
+             <img src="img/SPEECH_BUBBLE_2.png" alt="icon">
+             <p>${comments}</p>
+         </div>
+         <div class="date">
+             <p class="big-num">${day}</p>
+             <p>${month}</p>
+         </div>
+     `;
   element.innerHTML = blogContent;
   return (element);
 }
-function RenderFootItem(model, parsedDate) {
-  const {previewImg, title} = model;
+
+function filterBlogs(res) {
+  const latestBlogs = [];
+  const footerBlogs = [];
+
+  res.blogs.forEach((blog) => {
+    if (res.latest.includes(blog.id)) {
+      latestBlogs.push(blog);
+    } else {
+      footerBlogs.push(blog);
+    }
+  });
+
+  return {
+    latestBlogs,
+    footerBlogs,
+  };
+}
+
+function renderBlogs(blogs, container, create) {
+  blogs.forEach(((model) => {
+    container.appendChild(create(model));
+  }));
+}
+
+function parseDate(published) {
+  const date = new Date(published);
+  const locale = 'en-us';
+  const month = date.toLocaleString(locale, {month: 'short'});
+  const year = date.getFullYear();
+  const day = date.getDate();
+
+  return {day, month, year};
+
+}
+
+function renderBlogItem(model) {
+  const {previewImg, title, published} = model;
   const element = document.createElement('div');
+  const {month, day, year} = parseDate(published);
+  const date = `${month} ${day}, ${year}`;
   element.classList.add('footer-blog');
 
+  const blog = `
+    <div class="footer-blog-image">
+      <img src="${previewImg}" alt="">
+    </div>
+    <div class="footer-blog-text">
+      <p>${title}</p>
+      <h5 class="footer-blog-date">${date}</h5>
+    </div>
+  `;
 
-  const footBlogContent = `
-            <div>
-                <img src=${previewImg} alt="photo">
-            </div>
-            <div class="footer-blog-text">
-                <p>${title}</p>
-                <p class="footer-blog-date">${parsedDate[2]}, ${parsedDate[1]} ${parsedDate[0]}</p>
-            </div>
-    `;
-  element.innerHTML = footBlogContent;
+  element.innerHTML = blog;
   return (element);
 }
 
-fetch('http://localhost:3000/api/blogs')
-    .then((response) => response.json()
-        .then((data) => [data.latest, data.blogs.sort(function(a, b) {
-          let aDate = new Date(a.published);
-          let bDate = new Date(b.published); return bDate.getTime() - aDate.getTime();
-        })]))
-    .then(renderer);
+function fetchResponse(res) {
+  const {latestBlogs, footerBlogs} = filterBlogs(res);
 
-function renderer(res) {
-  res[1].filter(function(item, index) {
-    return res[0].includes(index);
-}).forEach(((model)=> {
-    let tempDate = new Date(model.published);
-    let locale = 'en-us';
-    let month = tempDate.toLocaleString(locale, {month: 'short'});
-    let theDate = [tempDate.getFullYear(), month, tempDate.getDate()];
-    eventsSection.appendChild(new RenderBlogItem(model, theDate));
-  }));
-  res[1].filter(function(item, index) {
-    return res[0].includes(index) === false;
-  }).forEach(((model)=> {
-    let tempDate = new Date(model.published);
-    let locale = 'en-us';
-    let month = tempDate.toLocaleString(locale, {month: 'short'});
-    let theDate = [tempDate.getFullYear(), month, tempDate.getDate()];
-    footerSection.appendChild(new RenderFootItem(model, theDate));
-  }));
+  const articlesSection = document.querySelector('.events-list');
+  const blogsSection = document.querySelector('.footer-blogs');
+
+  renderBlogs(latestBlogs, articlesSection, renderArticleItem);
+  renderBlogs(footerBlogs, blogsSection, renderBlogItem);
 }
+
+fetch('http://localhost:3000/api/blogs')
+    .then((response) => response.json())
+    .then(fetchResponse);
+
