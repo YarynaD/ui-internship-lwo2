c3785a02ce0c31025b325cd7984c83ddf09d9e1f
YarynaD <ydemkiv@griddynamics.com>
2018-12-14 19:02:29 +0200

    slider init

--- css-practice/task_3/slider.js
+++ css-practice/task_3/slider.js
@@ -1,39 +1,44 @@
 /* global document */
 
-(function() {
-  const sliderArray = [...document.querySelectorAll('.slider')];
-  const sldr = [...document.querySelectorAll('.slide-holder')];
-  let trFinished = 1;
-  sliderArray.forEach((el) => {
-    el.addEventListener('click', clickfunction, true);
-  });
-  sldr.forEach((el) => {
-    el.addEventListener('transitionend', function(event) {
-      trFinished = 1;
-    }, false);
-  });
+const initSlider = (id) => {
+  const slideEl = document.getElementById(id);
+  const leftArrow = slideEl.querySelector('.scroll-left');
+  const rightArrow = slideEl.querySelector('.scroll-right');
+  const scroller = slideEl.querySelector('.slide-holder');
 
-  function clickfunction(e) {
-    if (trFinished === 0) {
+
+  let counter = 0;
+
+  scroller.style.transform = 'translateX(' + (-107 * counter) + '%)';
+
+  leftArrow.addEventListener('click', () => {
+    if (counter <= 0) {
+      counter = 2;
+      scroller.classList.add('transition');
+      scroller.style.transform = 'translateX(' + (-98 * counter) + '%)';
       return;
     }
-    trFinished = 0;
-    let motion = -33;
-    if (e.target.classList.contains('scroll-left')) {
-      motion = 33;
+    scroller.classList.add('transition');
+    counter--;
+    scroller.style.transform = 'translateX(' + (-98 * counter) + '%)';
+  });
+
+  rightArrow.addEventListener('click', () => {
+    if (counter >= 2) {
+      counter = 0;
+      scroller.classList.add('transition');
+      scroller.style.transform = 'translateX(0%)';
+      return;
     }
-    let slides = e.target.parentNode.getElementsByClassName('slide-holder')[0];
-    let style = window.getComputedStyle(slides);
-    let trans = style.transform;
-    let numberPattern = /-*\d+/g;
-    let values = trans.match( numberPattern );
-    let computedTranslateX = values[4];
-    let xPercentage = (computedTranslateX / slides.offsetWidth) * 100;
-    let newX = xPercentage + motion;
-    if (newX > 4 || newX <-70) {
-      newX -= motion;
-    }
-    slides.style.transform = 'translateX(' + newX + '%)';
-  }
-})();
+    scroller.classList.add('transition');
+    counter++;
+    scroller.style.transform = 'translateX(' + (-98 * counter) + '%)';
+  });
+};
+
+
+window.onload = function() {
+  initSlider('review_slider');
+  initSlider('quotes_slider');
+};
 
